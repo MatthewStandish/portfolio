65db3b91975b9f1b1a7eef6a68c3f8a5ac948008
Matthew Standish <mwstandish@aol.com>
2023-03-30 11:41:29 -0400

    fixed carousel sizing and added fade in effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,23 @@ let mainPhoto = document.getElementById("mainPhoto");
 function updateImage(event) {
   let image = event.target;
 
-  mainPhoto.src = image.src;
+  mainPhoto.classList.add("fade-out");
+
+  image.classList.remove("fade-in");
+
+  void image.offsetWidth;
+
+  image.classList.add("fade-in");
+
+  setTimeout(function () {
+    mainPhoto.src = image.src;
+
+    mainPhoto.classList.remove("fade-out");
+  }, 500);
 }
 
 images.forEach(function (image) {
   image.addEventListener("click", updateImage);
+
+  image.classList.add("fade-in");
 });
