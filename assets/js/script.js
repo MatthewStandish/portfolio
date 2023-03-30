64be749397581d4bcbef05d254bba6a4b9b4d4e5
Matthew Standish <mwstandish@aol.com>
2023-03-30 17:00:33 -0400

    fixed script and carousel links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,22 @@
-// Use a more descriptive name for the variable containing the image elements
-const carouselImages = Array.from(
-  document.getElementsByClassName("imgCarousel")
-);
-
+const images = document.querySelectorAll(".imgCarousel");
 const mainPhoto = document.getElementById("mainPhoto");
+const mainPhotoLink = document.querySelector(".mainPhotoContainer a");
+const githubLink = document.getElementById("githubLink");
 
-function updateImage(event) {
-  const clickedImage = event.target;
-
-  mainPhoto.classList.add("fade-out");
-
-  clickedImage.classList.remove("fade-in");
-
-  void clickedImage.offsetHeight;
-
-  clickedImage.classList.add("fade-in");
-
-  setTimeout(function () {
-    mainPhoto.src = clickedImage.src;
-
-    mainPhoto.classList.remove("fade-out");
-
-    clickedImage.classList.add("fade-in");
-  }, 300);
-}
-
-carouselImages.forEach(function (image) {
-  image.addEventListener("click", updateImage);
-
-  image.classList.add("fade-in");
+images.forEach((image) => {
+  image.addEventListener("click", function () {
+    const src = this.getAttribute("src");
+    const githubLinkAttr = this.getAttribute("data-github-link");
+    const imageLinkAttr = this.getAttribute("data-image-link");
+    mainPhoto.setAttribute("src", src);
+    mainPhotoLink.setAttribute("href", imageLinkAttr);
+    githubLink.setAttribute("href", githubLinkAttr);
+    githubLink.children[0].setAttribute(
+      "src",
+      "./assets/images/github-mark.png"
+    );
+    githubLink.children[0].setAttribute("alt", `View ${this.alt} on Github`);
+    images.forEach((image) => image.classList.remove("active"));
+    this.classList.add("active");
+  });
 });
