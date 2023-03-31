76a38e7332d70fdb6e79063e83b5840c02416a16
Matthew Standish <mwstandish@aol.com>
2023-03-31 11:21:43 -0400

    working on media queries

--- assets/js/about-script.js
+++ assets/js/about-script.js
@@ -1,5 +1,4 @@
-const paragraph =
-  "As a 20-year-old with a passion for graphic design and web development, I am currently enrolled in a full stack web development bootcamp through Georgia Tech.\nBefore beginning the bootcamp, I earned a certificate as an Adobe Certified Associate, which solidified my interest in pursuing a career in technology. Throughout my education and professional experience, I have developed a strong work ethic and a dedication to personal and professional growth.\nI am excited to continue building upon my skills and expanding my knowledge base in web development as I progress through the bootcamp.\nWith a background in graphic design and a growing expertise in web development, I am eager to explore opportunities in the technology industry that will allow me to apply my creative talents and technical skills to real-world projects.\nI am passionate about leveraging my skills to make a meaningful impact in the field and am excited to discover new opportunities to achieve my goals and advance my career.";
+const paragraph = "";
 
 const typingContainer = document.getElementById("typing-container");
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ images.forEach((image) => {
     githubLink.setAttribute("href", githubLinkAttr);
     githubLink.children[0].setAttribute(
       "src",
-      "./assets/images/github-mark.png"
+      "./assets/images/github-mark-black.png"
     );
     githubLink.children[0].setAttribute("alt", `View ${this.alt} on Github`);
     images.forEach((image) => image.classList.remove("active"));
