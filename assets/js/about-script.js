ea36df02dd1c7bd6207ac1c07a39ad6595dd4215
Matthew Standish <mwstandish@aol.com>
2023-04-03 19:49:44 -0400

    working on links button

--- assets/js/about-script.js
+++ assets/js/about-script.js
@@ -1,8 +1,24 @@
-const paragraph = "My name is Matthew Standish";
-
+const paragraph =
+  "As a passionate 20-year-old pursuing a career in technology, I am currently enrolled in Georgia Tech's full-stack web development bootcamp.\nPrior to this, I earned an Adobe Certified Associate certificate, which solidified my interest in graphic design and web development.\nThrough my education and professional experience, I have developed a strong work ethic and a dedication to personal and professional growth.";
 const typingContainer = document.getElementById("typing-container");
+const btnEl = document.querySelector(".btn");
+const wrapperEl = document.querySelector(".wrapper");
 
-function typeParagraph() {
+const toggleOptions = () => {
+  const iconEl = btnEl.querySelector("i");
+
+  wrapperEl.classList.toggle("active");
+
+  if (iconEl.classList.contains("ri-share-line")) {
+    iconEl.classList.replace("ri-share-line", "ri-close-line");
+  } else {
+    iconEl.classList.replace("ri-close-line", "ri-share-line");
+  }
+};
+
+btnEl.addEventListener("click", toggleOptions);
+
+function typeParagraph(callback) {
   let i = 0;
   const interval = setInterval(function () {
     if (i <= paragraph.length) {
@@ -10,8 +26,24 @@ function typeParagraph() {
       i++;
     } else {
       clearInterval(interval);
+      if (callback) {
+        callback();
+      }
     }
-  }, 40);
+  }, 35);
 }
 
-setTimeout(typeParagraph, 1000);
+setTimeout(() => {
+  typeParagraph(() => {
+    wrapperEl.style.opacity = 0;
+    wrapperEl.style.display = "block";
+    let opacity = 0;
+    const interval = setInterval(() => {
+      opacity += 0.01;
+      wrapperEl.style.opacity = opacity;
+      if (opacity >= 1) {
+        clearInterval(interval);
+      }
+    }, 10);
+  });
+}, 1000);
